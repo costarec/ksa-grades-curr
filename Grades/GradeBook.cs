42ff8c6408af65c72a341ef687b4c3c08aefe404
Charles Costarella <costarec@uw.edu>
2018-02-18 17:08:27 -0800

    ksa-grades-curr

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // members of this class that store state or data
+
+        // members of this class that do things - actions - methods
+
+        // here is the replacement for the default ctor 
+        public GradeBook()
+        {
+            // create the collection of grades
+            grades = new List<float>();
+        }
+
+        // Add a grade to this gradebook 
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        // never assigned to and will always have an initial value of null
+        List<float> grades;
+    }
+}
+
---
+++ Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    // TODO - Our new GradeBook program needs some spec'd out requirements.
+    // We need an electronic GradeBook to read the scores if an individual student
+    // and then compute some simple statistics from the scores.
+    // The grades are entered as floating point numbers from 0 to 100, and
+    // the statistics should show us the highest grade, the lowest grade, and
+    // the average grade.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO- create a new grade book object to put test data into
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            // TODO - now we're going to eliminate the original gradebook in
+            GradeBook book2 = book;
+            book2.AddGrade(75);
+
+        }
+    }
+}
